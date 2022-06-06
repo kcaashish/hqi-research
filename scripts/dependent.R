b10f20512f02b77fe891daf8a256597e20314ac0
Aashish KC <kcaashish@gmail.com>
2022-06-06 11:49:58 +0545

    separate all the csv imports into its own script

--- scripts/dependent.R
+++ scripts/dependent.R
@@ -3,14 +3,8 @@ library(ggbiplot)
 # install_github("vqv/ggbiplot")
 library(psych)
 
-# importing the required csv files ----
-s00 <- read.csv("./data/raw/S00_rc_dta-csv copy.csv")
-s01 <- read.csv("./data/raw/S01_rc_dta-csv copy.csv")
-s02 <- read.csv("./data/raw/S02_rc_dta-csv copy.csv")
-s04 <- read.csv("./data/raw/S04_rc_dta-csv copy.csv")
-s06 <- read.csv("./data/raw/S06_rc_dta-csv copy.csv")
-s09 <- read.csv("./data/raw/S09_rc_dta-csv copy.csv")
-s12 <- read.csv("./data/raw/S12_rc_dta-csv copy.csv")
+# sourcing the required scripts ----
+source("./scripts/imports.R")
 
 # dependent vars ----
 dependent_vars <- s01 %>%
---
+++ scripts/imports.R
@@ -0,0 +1,8 @@
+# importing the required csv files ----
+s00 <- read.csv("./data/raw/S00_rc_dta-csv copy.csv")
+s01 <- read.csv("./data/raw/S01_rc_dta-csv copy.csv")
+s02 <- read.csv("./data/raw/S02_rc_dta-csv copy.csv")
+s04 <- read.csv("./data/raw/S04_rc_dta-csv copy.csv")
+s06 <- read.csv("./data/raw/S06_rc_dta-csv copy.csv")
+s09 <- read.csv("./data/raw/S09_rc_dta-csv copy.csv")
+s12 <- read.csv("./data/raw/S12_rc_dta-csv copy.csv")
