b16a186ac80fb0de3d018b5784d54e39779c5bc5
Aashish KC <kcaashish@gmail.com>
2022-06-06 13:15:36 +0545

    get stats of independent variables including regions for a national stat

--- scripts/independent.R
+++ scripts/independent.R
@@ -110,7 +110,7 @@ count <- independent_var %>%
   mutate(Variable = "Total Count")
 
 # country-wide stats of independent variables ----
-total <- independent_var %>% 
+total_no_region <- independent_var %>% 
   dplyr::summarise(across(all_of(vars[vars != "region"]), list(
     "Mean" = mean,
     "Sd." = sd,
@@ -124,7 +124,36 @@ total <- independent_var %>%
   pivot_wider(names_from = c("Stat"), names_glue = "Total_{Stat}", values_from = "Val")
 
 # bind regional stats with country-wide stats and count values ----
-grand_total <- left_join(regional, total, by = "Variable")
+grand_total_no_region <- left_join(regional, total_no_region, by = "Variable")
 options(scipen = 999)
-grand_total_count <- bind_rows(grand_total, count)
+grand_total_no_region_count <- bind_rows(grand_total_no_region, count)
 
+# giving 3 different columns for regions ----
+independent_var_with_region <- independent_var %>% 
+  mutate(
+    himalaya = if_else(region == "Himalaya", 1, 0),
+    hill = if_else(region == "Hill", 1, 0),
+    terai = if_else(region == "Terai", 1, 0)
+  ) %>% 
+  select(!c("region"))
+
+# country-wide stats of independent variables including regions ----
+total_observation <- independent_var_with_region %>% 
+  dplyr::summarise(across(all_of(names(independent_var_with_region)[-c(1:2)]), list(
+    "Mean" = mean,
+    "Sd." = sd,
+    "Min." = min,
+    "Max." = max
+  ), .names = "{.col}-{.fn}")
+  ) %>% 
+  pivot_longer(everything(), names_to = "Variables", values_to = "Val") %>% 
+  separate(Variables, sep = "-", into = c("Variable", "Stat")) %>% 
+  group_by(Variable) %>% 
+  pivot_wider(names_from = c("Stat"), names_glue = "Total_{Stat}", values_from = "Val")
+
+# total observation with count ----
+total_observation_count <- total_observation %>% 
+  bind_rows(tibble(Variable = "Total Observations", Total_Mean = nrow(independent_var_with_region)))
+
+# save dataset ----
+saveRDS(independent_var_with_region, file = "./data/processed/independent.RDS")
