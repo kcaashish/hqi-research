16e9bf6df1728762345b04cc0e0e480e8c6e0a47
Aashish KC <kcaashish@gmail.com>
2022-06-09 00:09:21 +0545

    get lineral regression models and perform stepAIC to get stepwise reg models

--- scripts/regression.R
+++ scripts/regression.R
@@ -19,34 +19,62 @@ national_model <- lm(HQI ~ sex + age + caste + marital + can_read + can_write + 
 summary(national_model)
 
 ### stepwise regression model ----
-national_step <- MASS::stepAIC(national_model, direction = "both", trace = FALSE)
-national_step$anova
-summary(national_step)
+national_model_f <- MASS::stepAIC(national_model, direction = "both", trace = FALSE)
+national_model_f$anova
+summary(national_model_f)
 
 
 # himalaya region equation ----
 him_variables <- left_join(
-  independent %>% filter(himalaya == 1) %>% select(-c(himalaya, hill, terai)),
+  independent_no %>% filter(region == "Himalaya") %>% select(-region),
   hqi_data %>% select(psu, hhld, HQI),
   by = c("psu", "hhld")
 )
-himalaya_model <- lm(HQI ~ . - psu - hhld, data = him_variables)
+
+### fit full model ----
+himalaya_model <- lm(HQI ~ sex + age + caste + marital + can_read + can_write + ever_school + 
+                       grade_comp + tec_voc_training + fam_size +
+                       own_land_own + other_land_own + own_land_other + hhg_tot30
+                       , data = him_variables)
 summary(himalaya_model)
+
+### stepwise regression model ----
+himalaya_model_f <- MASS::stepAIC(himalaya_model, direction = "both", trace = FALSE)
+himalaya_model_f$anova
+summary(himalaya_model_f)
 
 # hill region equation ----
 hill_variables <- left_join(
-  independent %>% filter(hill == 1) %>% select(-c(himalaya, hill, terai)),
+  independent_no %>% filter(region == "Hill") %>% select(-region),
   hqi_data %>% select(psu, hhld, HQI),
   by = c("psu", "hhld")
 )
-hill_model <- lm(HQI ~ . - psu - hhld, data = hill_variables)
+
+### fit full model ----
+hill_model <- lm(HQI ~ sex + age + caste + marital + can_read + can_write + ever_school + 
+                       grade_comp + tec_voc_training + fam_size +
+                       own_land_own + other_land_own + own_land_other + hhg_tot30, data = hill_variables)
 summary(hill_model)
+
+### stepwise regression model ----
+hill_model_f <- MASS::stepAIC(hill_model, direction = "both", trace = FALSE)
+hill_model_f$anova
+summary(hill_model_f)
 
 # terai region equation ----
 ter_variables <- left_join(
-  independent %>% filter(terai == 1) %>% select(-c(himalaya, hill, terai)),
+  independent_no %>% filter(region == "Terai") %>% select(-region),
   hqi_data %>% select(psu, hhld, HQI),
   by = c("psu", "hhld")
 )
-terai_model <- lm(HQI ~ . - psu - hhld, data = ter_variables)
+
+### fit full model ----
+terai_model <- lm(HQI ~ sex + age + caste + marital + can_read + can_write + ever_school + 
+                   grade_comp + tec_voc_training + fam_size +
+                   own_land_own + other_land_own + own_land_other + hhg_tot30, data = ter_variables)
 summary(terai_model)
+
+### stepwise regression model ----
+terai_model_f <- MASS::stepAIC(terai_model, direction = "both", trace = FALSE)
+terai_model_f$anova
+summary(terai_model_f)
