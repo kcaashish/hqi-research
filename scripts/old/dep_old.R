6ae44eaeb196c6c8696216fb9f48b09fdca6b6e7
Aashish KC <kcaashish@gmail.com>
2022-06-24 20:30:13 +0545

    add province into the data

--- scripts/old/dep_old.R
+++ scripts/old/dep_old.R
@@ -24,9 +24,18 @@ dep_old_f <- to_factor(dep_old, levels = "labels") %>%
                   xstra == 100 ~ 1,
                   xstra %in% c(218, 219, 221, 222, 223, 224, 225) ~ 2,
                   xstra %in% c(310, 321, 322, 323, 324, 325) ~ 3
+                ),
+                province = case_when(
+                  v00_dist %in% c(1:14) ~ 1,
+                  v00_dist %in% c(15:19, 32:34) ~ 2,
+                  v00_dist %in% c(20:31, 35) ~ 3,
+                  v00_dist %in% c(36:45) ~ 4,
+                  v00_dist %in% c(46:58) ~ 5,
+                  v00_dist %in% c(59:66) ~ 6,
+                  v00_dist %in% c(67:75) ~ 7
                 )) %>% 
               dplyr::mutate(region = factor(region, labels = c("Himalaya", "Hill", "Terai"))) %>% 
-              select(psu = xhpsu, hhld = xhnum, region),
+              select(psu = xhpsu, hhld = xhnum, region, province),
             by = c("psu", "hhld"))
 
 # changing "No outside walls" to "Other material" for outerwall ----
@@ -36,7 +45,7 @@ str(dep_old_f)
 
 # arrange the factor levels, in higher = better order
 arranged_dep_ini <-
-  as_tibble(lapply(dep_old_f[-c(1, 2, ncol(dep_old_f))], function(x)
+  as_tibble(lapply(dep_old_f[-c(1, 2, 11, 12)], function(x)
     factor(x, levels = rev(levels(
       x
     ))))) %>%
@@ -142,12 +151,12 @@ KMO(alpha_hqi_rev)
 
 # calculate the HQI and add region before saving ----
 hqi <- arranged_dep_num %>%
-  bind_cols(., dep_old_f[ncol(dep_old_f)]) %>% 
+  bind_cols(., dep_old_f[c(11, 12)]) %>% 
   mutate(HQI = rowSums(.[1:8])) %>% 
   bind_cols(., dep_old_f[c(1, 2)])
 
 hqi_vars <- arranged_dep %>% 
-  bind_cols(., hqi[tail(names(hqi), 4)])
+  bind_cols(., hqi[tail(names(hqi), 5)])
 
 # save dependent vars data ----
 saveRDS(hqi_vars, file = "./data/old/processed/dep_old_vars.RDS")
